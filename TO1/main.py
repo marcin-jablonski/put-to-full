ad9e45fa4fd26600c8d61b62af0d102d927e654a
Marcin Jabłoński <jablon1994@gmail.com>
2017-11-19 22:23:16 +0100

    Added regret version, some quality-of-life code for results

--- TO1/main.py
+++ TO1/main.py
@@ -118,8 +118,68 @@ def cycle_expansion(nodes, starting_node_index=0):
     return cycle, final_value
 
 
+def find_best_regret_expansion(nodes, cycle, cycle_values):
+    best_node = None
+    best_node_regret = None
+    broken_edge_index = None
+
+    for node in nodes:
+        first_best_edge_index = None
+        second_best_edge_index = None
+        first_best_edge_score = 0
+        second_best_edge_score = 0
+
+        for edge_index in range(len(cycle) - 1):
+            new_cycle_values = cycle_values.copy()
+            del new_cycle_values[2*edge_index]
+            new_cycle_values[2*edge_index:2*edge_index] = [-distance(node, cycle[edge_index]) * COST_WEIGHT, node.gain, -distance(node, cycle[edge_index + 1]) * COST_WEIGHT]
+            edge_break_result = sum(new_cycle_values) - sum(cycle_values)
+
+            if first_best_edge_index is None or edge_break_result > first_best_edge_score:
+                second_best_edge_index = first_best_edge_index
+                second_best_edge_score = first_best_edge_score
+                first_best_edge_index = edge_index
+                first_best_edge_score = edge_break_result
+            elif second_best_edge_index is None or edge_break_result > second_best_edge_score:
+                second_best_edge_index = edge_index
+                second_best_edge_score = edge_break_result
+
+        if first_best_edge_index is not None and first_best_edge_score > 0:
+            node_regret = first_best_edge_score - second_best_edge_score
+
+            if best_node is None or node_regret > best_node_regret:
+                best_node = node
+                best_node_regret = node_regret
+                broken_edge_index = first_best_edge_index
+
+    return best_node, broken_edge_index
+
+
+def insert_node_with_breaking_edge(node, edge_index, cycle, cycle_values):
+    cycle.insert(edge_index + 1, node)
+    del cycle_values[2 * edge_index]
+    cycle_values[2 * edge_index:2 * edge_index] = [-distance(node, cycle[edge_index]) * COST_WEIGHT, node.gain, -distance(node, cycle[edge_index + 1]) * COST_WEIGHT]
+    return cycle, cycle_values
+
+
 def cycle_expansion_with_regret(nodes, starting_node_index=0):
-    raise NotImplementedError
+    first_node = nodes[starting_node_index]
+    nodes.remove(first_node)
+    second_node, second_node_result = find_nearest_neighbour(first_node, nodes)
+    nodes.remove(second_node)
+    cycle = [first_node, second_node, first_node]
+    cycle_values = [second_node_result - second_node.gain, second_node.gain, second_node_result - second_node.gain, first_node.gain]
+
+    while True:
+        node, edge_index = find_best_regret_expansion(nodes, cycle, cycle_values)
+
+        if node is None:
+            break
+
+        nodes.remove(node)
+        cycle, cycle_values = insert_node_with_breaking_edge(node, edge_index, cycle, cycle_values)
+
+    return cycle, sum(cycle_values)
 
 
 def print_result(nodes, result_nodes, result, title):
@@ -143,20 +203,42 @@ def print_result(nodes, result_nodes, result, title):
 
 def main():
     nodes = read_data("./data")
-    best_solution = None
-    best_result = None
+    best_nearest_neighbour_solution = None
+    best_nearest_neighbour_result = None
+    nearest_neighbour_results = []
+    best_cycle_expansion_solution = None
+    best_cycle_expansion_result = None
+    cycle_expansion_results = []
+    best_cycle_expansion_with_regret_solution = None
+    best_cycle_expansion_with_regret_result = None
+    cycle_expansion_with_regret_results = []
+
     for starting_index in range(0, len(nodes)):
         solution = nearest_neighbour(nodes.copy(), starting_index)
-        #solution = cycle_expansion(nodes.copy(), starting_index)
-        if best_solution is None or solution[1] > best_result:
-            best_solution = solution[0]
-            best_result = solution[1]
-    # print(nearest_neighbour(nodes.copy(), starting_index)[1])
-    # print(cycle_expansion(nodes.copy(), starting_index)[1])
-    # cycle_expansion_with_regret(nodes.copy(), starting_index)
-    print(list(map(lambda node: node.id, best_solution)))
-    #print_result(nodes, best_solution, best_result, 'Cycle expansion')
-    print_result(nodes, best_solution, best_result, 'Nearest neighbour')
+        nearest_neighbour_results.append(solution[1])
+        if best_nearest_neighbour_solution is None or solution[1] > best_nearest_neighbour_result:
+            best_nearest_neighbour_solution = solution[0]
+            best_nearest_neighbour_result = solution[1]
+
+        solution = cycle_expansion(nodes.copy(), starting_index)
+        cycle_expansion_results.append(solution[1])
+        if best_cycle_expansion_solution is None or solution[1] > best_cycle_expansion_result:
+            best_cycle_expansion_solution = solution[0]
+            best_cycle_expansion_result = solution[1]
+
+        solution = cycle_expansion_with_regret(nodes.copy(), starting_index)
+        cycle_expansion_with_regret_results.append(solution[1])
+        if best_cycle_expansion_with_regret_solution is None or solution[1] > best_cycle_expansion_with_regret_result:
+            best_cycle_expansion_with_regret_solution = solution[0]
+            best_cycle_expansion_with_regret_result = solution[1]
+
+    print_result(nodes, best_nearest_neighbour_solution, best_nearest_neighbour_result, 'Nearest neighbour')
+    print('Nearest neigbour - best: {}, worst: {}, average: {}'.format(best_nearest_neighbour_result, min(nearest_neighbour_results), np.mean(nearest_neighbour_results)))
+    print_result(nodes, best_cycle_expansion_solution, best_cycle_expansion_result, 'Cycle expansion')
+    print('Cycle expansion - best: {}, worst: {}, average: {}'.format(best_cycle_expansion_result, min(cycle_expansion_results), np.mean(cycle_expansion_results)))
+    print_result(nodes, best_cycle_expansion_with_regret_solution, best_cycle_expansion_with_regret_result, 'Cycle expansion with regret')
+    print('Cycle expansion with regret - best: {}, worst: {}, average: {}'.format(best_cycle_expansion_with_regret_result, min(cycle_expansion_with_regret_results), np.mean(cycle_expansion_with_regret_results)))
+
 
 
 main()
